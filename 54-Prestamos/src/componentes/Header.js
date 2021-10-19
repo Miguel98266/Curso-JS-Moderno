1220a05efe330daeb6ac6a88bae4f862b6f1c919
Miguel98266 <35313138+Miguel98266@users.noreply.github.com>
2021-10-18 22:44:05 -0500

    v1

---
+++ 54-Prestamos/src/componentes/Header.js
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import React from "react";
+
+// function Header({titulo}) {
+//   console.log(titulo);
+
+//   return (
+//     <Fragment>
+//       <h1>{titulo}</h1>
+      
+//     </Fragment>
+//   );
+// }
+
+const Header = ({titulo}) => {
+    return ( 
+        <Fragment>
+        <h1>{titulo}</h1>
+      </Fragment>
+     );
+}
+ 
+
+
+export default Header;
